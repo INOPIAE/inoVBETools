efd9455a686553be184c0585943dc0fa12fe622c
INOPIAE <m.maengel@inopiae.de>
2022-12-12 15:22:28 +0100

    upd: move LineNumbering function to class

--- inoVBETools/class/Connect.vb
+++ inoVBETools/class/Connect.vb
@@ -25,6 +25,7 @@ Public Class Connect
 
     Private ClsIndent As New Indentation
     Private ClsVBEHandling As New VBEHandling
+    Private ClsLineNumbering As New LineNumbering
 
     Public Sub OnConnection(Application As Object, ConnectMode As ext_ConnectMode, AddInInst As Object, ByRef custom As Array) Implements IDTExtensibility2.OnConnection
         Try
@@ -93,7 +94,7 @@ Public Class Connect
     End Sub
 
     Private Sub _MyLineNummeringButton1_Click(Ctrl As CommandBarButton, ByRef CancelDefault As Boolean) Handles _MyLineNummeringButton1.Click
-        AddLineNumbersToComponent(_VBE.ActiveCodePane.CodeModule)
+        ClsLineNumbering.AddLineNumbersToComponent(_VBE.ActiveCodePane.CodeModule)
     End Sub
 
     Private Sub _MyErrorHandling_Click(Ctrl As CommandBarButton, ByRef CancelDefault As Boolean) Handles _MyErrorHandling.Click
@@ -117,71 +118,71 @@ Public Class Connect
 
     End Sub
 
-    Public Function AddLineNumbersToComponent(vbaCodeModule As CodeModule, Optional blnNoNumber As Boolean = False, Optional blnEachProcedure As Boolean = True) As Long
-        ' returns total line numbers added to code of a single code object as passed to the function
-        Dim intLine As Integer
-        Dim intColumn As Integer, intLineCounter As Integer
-        Dim strModulname As String = vbNullString
-        Dim bolUnderscore As Boolean, bolSelect As Boolean
-        Dim lngCount As Long
+    'Public Function AddLineNumbersToComponent(vbaCodeModule As CodeModule, Optional blnNoNumber As Boolean = False, Optional blnEachProcedure As Boolean = True) As Long
+    '    ' returns total line numbers added to code of a single code object as passed to the function
+    '    Dim intLine As Integer
+    '    Dim intColumn As Integer, intLineCounter As Integer
+    '    Dim strModulname As String = vbNullString
+    '    Dim bolUnderscore As Boolean, bolSelect As Boolean
+    '    Dim lngCount As Long
 
-        With vbaCodeModule
-            For intLine = .CountOfDeclarationLines + 1 To .CountOfLines
-                If .Lines(intLine, 1).Trim <> vbNullString And If(.Lines(intLine, 1).Trim <> vbNullString, .Lines(intLine, 1).Trim.First <> "'", False) Then
-                    If .ProcOfLine(intLine, 0) <> strModulname Then
-                        strModulname = .ProcOfLine(intLine, 4)
-                        If blnEachProcedure = True Then
-                            intLineCounter = 0
-                        End If
-                        If .Lines(intLine, 1).Trim.Last = "_" Then
-                            bolUnderscore = True
-                        Else
-                            bolUnderscore = False
-                        End If
-                    Else
-                        If "End Sub End Function End Property".Contains(.Lines(intLine, 1)) = False Then
-                            If Not bolUnderscore And Not bolSelect Then
-                                If .Lines(intLine, 1).Trim.Last = "_" Then bolUnderscore = True
-                                If .Lines(intLine, 1).Contains("Select Case") Then bolSelect = True
-                                If IsNumeric(.Lines(intLine, 1).Substring(0, 1)) Then
-                                    For intColumn = 1 To .Lines(intLine, 1).Length
-                                        If Not IsNumeric(.Lines(intLine, 1).Substring(0, intColumn)) Then
-                                            Exit For
-                                        End If
-                                    Next
-                                    .ReplaceLine(intLine, StrDup(intColumn, " ") & .Lines(intLine, 1).Substring(intColumn - 1))
-                                End If
-                                intLineCounter += 1
-                                If blnNoNumber = False Then
-                                    If .Lines(intLine, 1).Length > 4 Then
-                                        For intColumn = 0 To 3
-                                            If .Lines(intLine, 1).Substring(intColumn, 1) <> " " Then
-                                                Exit For
-                                            End If
-                                        Next
-                                        .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Substring(intColumn))
-                                    Else
-                                        .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Trim)
-                                    End If
+    '    With vbaCodeModule
+    '        For intLine = .CountOfDeclarationLines + 1 To .CountOfLines
+    '            If .Lines(intLine, 1).Trim <> vbNullString And If(.Lines(intLine, 1).Trim <> vbNullString, .Lines(intLine, 1).Trim.First <> "'", False) Then
+    '                If .ProcOfLine(intLine, 0) <> strModulname Then
+    '                    strModulname = .ProcOfLine(intLine, 4)
+    '                    If blnEachProcedure = True Then
+    '                        intLineCounter = 0
+    '                    End If
+    '                    If .Lines(intLine, 1).Trim.Last = "_" Then
+    '                        bolUnderscore = True
+    '                    Else
+    '                        bolUnderscore = False
+    '                    End If
+    '                Else
+    '                    If "End Sub End Function End Property".Contains(.Lines(intLine, 1)) = False Then
+    '                        If Not bolUnderscore And Not bolSelect Then
+    '                            If .Lines(intLine, 1).Trim.Last = "_" Then bolUnderscore = True
+    '                            If .Lines(intLine, 1).Contains("Select Case") Then bolSelect = True
+    '                            If IsNumeric(.Lines(intLine, 1).Substring(0, 1)) Then
+    '                                For intColumn = 1 To .Lines(intLine, 1).Length
+    '                                    If Not IsNumeric(.Lines(intLine, 1).Substring(0, intColumn)) Then
+    '                                        Exit For
+    '                                    End If
+    '                                Next
+    '                                .ReplaceLine(intLine, StrDup(intColumn, " ") & .Lines(intLine, 1).Substring(intColumn - 1))
+    '                            End If
+    '                            intLineCounter += 1
+    '                            If blnNoNumber = False Then
+    '                                If .Lines(intLine, 1).Length > 4 Then
+    '                                    For intColumn = 0 To 3
+    '                                        If .Lines(intLine, 1).Substring(intColumn, 1) <> " " Then
+    '                                            Exit For
+    '                                        End If
+    '                                    Next
+    '                                    .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Substring(intColumn))
+    '                                Else
+    '                                    .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Trim)
+    '                                End If
 
-                                    lngCount += 1
-                                End If
-                            Else
-                                If .Lines(intLine, 1).Trim.Last <> "_" Then bolUnderscore = False
-                                If .Lines(intLine, 1).Contains("Case") Then bolSelect = False
-                            End If
-                        Else
-                            strModulname = vbNullString
-                        End If
-                    End If
-                End If
-            Next
-        End With
-        AddLineNumbersToComponent = lngCount
-    End Function
+    '                                lngCount += 1
+    '                            End If
+    '                        Else
+    '                            If .Lines(intLine, 1).Trim.Last <> "_" Then bolUnderscore = False
+    '                            If .Lines(intLine, 1).Contains("Case") Then bolSelect = False
+    '                        End If
+    '                    Else
+    '                        strModulname = vbNullString
+    '                    End If
+    '                End If
+    '            End If
+    '        Next
+    '    End With
+    '    AddLineNumbersToComponent = lngCount
+    'End Function
 
     Private Sub _MyLineNummeringButton2_Click(Ctrl As CommandBarButton, ByRef CancelDefault As Boolean) Handles _MyLineNummeringButton2.Click
-        AddLineNumbersToComponent(_VBE.ActiveCodePane.CodeModule, True)
+        ClsLineNumbering.AddLineNumbersToComponent(_VBE.ActiveCodePane.CodeModule, True)
     End Sub
 
     Private Sub _MySettings_Click(Ctrl As CommandBarButton, ByRef CancelDefault As Boolean) Handles _MySettings.Click
---
+++ inoVBETools/class/LineNumbering.vb
@@ -0,0 +1,66 @@
+﻿Imports Microsoft.Vbe.Interop
+
+Public Class LineNumbering
+    Public Function AddLineNumbersToComponent(vbaCodeModule As CodeModule, Optional blnNoNumber As Boolean = False, Optional blnEachProcedure As Boolean = True) As Long
+        ' returns total line numbers added to code of a single code object as passed to the function
+        Dim intLine As Integer
+        Dim intColumn As Integer, intLineCounter As Integer
+        Dim strModulname As String = vbNullString
+        Dim bolUnderscore As Boolean, bolSelect As Boolean
+        Dim lngCount As Long
+
+        With vbaCodeModule
+            For intLine = .CountOfDeclarationLines + 1 To .CountOfLines
+                If .Lines(intLine, 1).Trim <> vbNullString And If(.Lines(intLine, 1).Trim <> vbNullString, .Lines(intLine, 1).Trim.First <> "'", False) Then
+                    If .ProcOfLine(intLine, 0) <> strModulname Then
+                        strModulname = .ProcOfLine(intLine, 4)
+                        If blnEachProcedure = True Then
+                            intLineCounter = 0
+                        End If
+                        If .Lines(intLine, 1).Trim.Last = "_" Then
+                            bolUnderscore = True
+                        Else
+                            bolUnderscore = False
+                        End If
+                    Else
+                        If "End Sub End Function End Property".Contains(.Lines(intLine, 1)) = False Then
+                            If Not bolUnderscore And Not bolSelect Then
+                                If .Lines(intLine, 1).Trim.Last = "_" Then bolUnderscore = True
+                                If .Lines(intLine, 1).Contains("Select Case") Then bolSelect = True
+                                If IsNumeric(.Lines(intLine, 1).Substring(0, 1)) Then
+                                    For intColumn = 1 To .Lines(intLine, 1).Length
+                                        If Not IsNumeric(.Lines(intLine, 1).Substring(0, intColumn)) Then
+                                            Exit For
+                                        End If
+                                    Next
+                                    .ReplaceLine(intLine, StrDup(intColumn, " ") & .Lines(intLine, 1).Substring(intColumn - 1))
+                                End If
+                                intLineCounter += 1
+                                If blnNoNumber = False Then
+                                    If .Lines(intLine, 1).Length > 4 Then
+                                        For intColumn = 0 To 3
+                                            If .Lines(intLine, 1).Substring(intColumn, 1) <> " " Then
+                                                Exit For
+                                            End If
+                                        Next
+                                        .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Substring(intColumn))
+                                    Else
+                                        .ReplaceLine(intLine, intLineCounter.ToString.PadRight(4) & .Lines(intLine, 1).Trim)
+                                    End If
+
+                                    lngCount += 1
+                                End If
+                            Else
+                                If .Lines(intLine, 1).Trim.Last <> "_" Then bolUnderscore = False
+                                If .Lines(intLine, 1).Contains("Case") Then bolSelect = False
+                            End If
+                        Else
+                            strModulname = vbNullString
+                        End If
+                    End If
+                End If
+            Next
+        End With
+        AddLineNumbersToComponent = lngCount
+    End Function
+End Class
