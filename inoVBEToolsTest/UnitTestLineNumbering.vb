d66c5328628539330a8be6779fbab585c7a22768
INOPIAE <m.maengel@inopiae.de>
2022-12-26 05:14:05 +0100

    upd: add namespace

--- inoVBEToolsTest/UnitTestLineNumbering.vb
+++ inoVBEToolsTest/UnitTestLineNumbering.vb
@@ -1,138 +1,140 @@
 ﻿Imports inoVBETools
 Imports Xunit
 
-Public Class UnitTestLineNumbering
-    Private ClsLineNumbering As New LineNumbering
+Namespace inoVBEToolsTest
+    Public Class UnitTestLineNumbering
+        Private ClsLineNumbering As New LineNumbering
 
-    <Fact>
-    Sub TestLineNumberingIf()
-        Dim strStart As String
+        <Fact>
+        Sub TestLineNumberingIf()
+            Dim strStart As String
 
-        strStart = "    If test = 1 Then" & vbCrLf _
+            strStart = "    If test = 1 Then" & vbCrLf _
             & "       test = 2" & vbCrLf _
             & "    Else" & vbCrLf _
             & "        test = 2" & vbCrLf _
             & "    End If"
 
-        Dim strResult As String
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
+            Dim strResult As String
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
 
-        Dim strTest As String = "1   If test = 1 Then" & vbCrLf _
+            Dim strTest As String = "1   If test = 1 Then" & vbCrLf _
             & "2      test = 2" & vbCrLf _
             & "3   Else" & vbCrLf _
             & "4       test = 2" & vbCrLf _
             & "5   End If"
-        Assert.Equal(strTest, strResult)
+            Assert.Equal(strTest, strResult)
 
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
-        Assert.Equal(strStart, strResult)
-    End Sub
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
+            Assert.Equal(strStart, strResult)
+        End Sub
 
-    <Fact>
-    Sub TestLineNumberingSelect()
-        Dim strStart As String
+        <Fact>
+        Sub TestLineNumberingSelect()
+            Dim strStart As String
 
-        strStart = "    Select Case foo" & vbCrLf _
+            strStart = "    Select Case foo" & vbCrLf _
                 & "        Case 1" & vbCrLf _
                 & "            test = 1" & vbCrLf _
                 & "        Case 2" & vbCrLf _
                 & "            test = 2" & vbCrLf _
                 & "    End Select"
 
-        Dim strResult As String
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
+            Dim strResult As String
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
 
-        Dim strTest As String = "1   Select Case foo" & vbCrLf _
+            Dim strTest As String = "1   Select Case foo" & vbCrLf _
                 & "        Case 1" & vbCrLf _
                 & "2           test = 1" & vbCrLf _
                 & "        Case 2" & vbCrLf _
                 & "3           test = 2" & vbCrLf _
                 & "4   End Select"
-        Assert.Equal(strTest, strResult)
+            Assert.Equal(strTest, strResult)
 
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
-        Assert.Equal(strStart, strResult)
-    End Sub
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
+            Assert.Equal(strStart, strResult)
+        End Sub
 
-    <Fact>
-    Sub TestLineNumberingComment()
-        Dim strStart As String
+        <Fact>
+        Sub TestLineNumberingComment()
+            Dim strStart As String
 
-        strStart = "    Test = 1" & vbCrLf _
+            strStart = "    Test = 1" & vbCrLf _
                 & "     'Test = 2" & vbCrLf _
                 & "     Test = 3" & vbCrLf _
                 & "     Test = 4 ' Comment" & vbCrLf _
                 & "' Test 5"
 
-        Dim strResult As String
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
+            Dim strResult As String
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
 
-        Dim strTest As String = "1   Test = 1" & vbCrLf _
+            Dim strTest As String = "1   Test = 1" & vbCrLf _
                 & "     'Test = 2" & vbCrLf _
                 & "2    Test = 3" & vbCrLf _
                 & "3    Test = 4 ' Comment" & vbCrLf _
                 & "' Test 5"
-        Assert.Equal(strTest, strResult)
+            Assert.Equal(strTest, strResult)
 
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
-        Assert.Equal(strStart, strResult)
-    End Sub
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
+            Assert.Equal(strStart, strResult)
+        End Sub
 
-    <Fact>
-    Sub TestLineNumberingBlankLines()
+        <Fact>
+        Sub TestLineNumberingBlankLines()
 
-        Dim strStart As String
+            Dim strStart As String
 
-        strStart = "    Test = 1" & vbCrLf _
+            strStart = "    Test = 1" & vbCrLf _
                 & "" & vbCrLf _
                 & "     Test = 3" & vbCrLf _
                 & "     " & vbCrLf _
                 & "     Test = 4 ' Comment" & vbCrLf _
                 & "     Test 5"
 
-        Dim strResult As String
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
-        Debug.Print(strResult)
-        Dim strTest As String = "1   Test = 1" & vbCrLf _
+            Dim strResult As String
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strStart)
+            Debug.Print(strResult)
+            Dim strTest As String = "1   Test = 1" & vbCrLf _
                 & "" & vbCrLf _
                 & "2    Test = 3" & vbCrLf _
                 & "     " & vbCrLf _
                 & "3    Test = 4 ' Comment" & vbCrLf _
                 & "4    Test 5"
-        Assert.Equal(strTest, strResult)
+            Assert.Equal(strTest, strResult)
 
-        strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
-        Assert.Equal(strStart, strResult)
-    End Sub
+            strResult = ClsLineNumbering.AddLineNumbersToCurrentProcedure(strResult, True)
+            Assert.Equal(strStart, strResult)
+        End Sub
 
-    <Fact>
-    Sub TestLineNumberingExists()
+        <Fact>
+        Sub TestLineNumberingExists()
 
-        Dim strStart As String
+            Dim strStart As String
 
-        strStart = "    Test = 1" & vbCrLf _
+            strStart = "    Test = 1" & vbCrLf _
                 & "" & vbCrLf _
                 & "     Test = 3" & vbCrLf _
                 & "     " & vbCrLf _
                 & "     Test = 4 ' Comment" & vbCrLf _
                 & "     Test 5"
 
-        Assert.False(ClsLineNumbering.HasLineNumbersInCode(strStart))
+            Assert.False(ClsLineNumbering.HasLineNumbersInCode(strStart))
 
-        strStart = "1    Test = 1" & vbCrLf _
+            strStart = "1    Test = 1" & vbCrLf _
                & "" & vbCrLf _
                & "     Test = 3" & vbCrLf _
                & "     " & vbCrLf _
                & "     Test = 4 ' Comment" & vbCrLf _
                & "     Test 5"
-        Assert.True(ClsLineNumbering.HasLineNumbersInCode(strStart))
+            Assert.True(ClsLineNumbering.HasLineNumbersInCode(strStart))
 
-        strStart = "'    Test = 1" & vbCrLf _
+            strStart = "'    Test = 1" & vbCrLf _
                & "1  Test=1a" & vbCrLf _
                & "     Test = 3" & vbCrLf _
                & "     " & vbCrLf _
                & "     Test = 4 ' Comment" & vbCrLf _
                & "     Test 5"
-        Assert.True(ClsLineNumbering.HasLineNumbersInCode(strStart))
-    End Sub
-End Class
+            Assert.True(ClsLineNumbering.HasLineNumbersInCode(strStart))
+        End Sub
+    End Class
+End Namespace
--- inoVBEToolsTest/UnitTestVBEHandling.vb
+++ inoVBEToolsTest/UnitTestVBEHandling.vb
@@ -4,98 +4,100 @@ Imports inoVBETools
 Imports inoVBETools.VBEHandling
 Imports Xunit
 
-Public Class UnitTestVBEHandling
+Namespace inoVBEToolsTest
+    Public Class UnitTestVBEHandling
 
-    Private ClsVBEH As New VBEHandling
+        Private ClsVBEH As New VBEHandling
 
-    Private TestFolder As String = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("bin\Debug\net6.0", "TestData")
+        Private TestFolder As String = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("bin\Debug\net6.0", "TestData")
 
-    <Fact>
-    Sub TestAddProjectEntry()
-        ClsVBEH.ProjectEntries.Clear()
+        <Fact>
+        Sub TestAddProjectEntry()
+            ClsVBEH.ProjectEntries.Clear()
 
-        Assert.Equal(0, ClsVBEH.ProjectEntries.Count)
+            Assert.Equal(0, ClsVBEH.ProjectEntries.Count)
 
-        ClsVBEH.ProjectAdd("A", "CDA")
+            ClsVBEH.ProjectAdd("A", "CDA")
 
-        Assert.Equal(1, ClsVBEH.ProjectEntries.Count)
-        Assert.Equal("A", ClsVBEH.ProjectEntries(0).ProjectName)
-        Assert.Equal("CDA", ClsVBEH.ProjectEntries(0).CodeDirectrory)
+            Assert.Equal(1, ClsVBEH.ProjectEntries.Count)
+            Assert.Equal("A", ClsVBEH.ProjectEntries(0).ProjectName)
+            Assert.Equal("CDA", ClsVBEH.ProjectEntries(0).CodeDirectrory)
 
-        ClsVBEH.ProjectAdd("B", "CDB")
+            ClsVBEH.ProjectAdd("B", "CDB")
 
-        Assert.Equal(2, ClsVBEH.ProjectEntries.Count)
-        Assert.Equal("B", ClsVBEH.ProjectEntries(1).ProjectName)
-        Assert.Equal("CDB", ClsVBEH.ProjectEntries(1).CodeDirectrory)
+            Assert.Equal(2, ClsVBEH.ProjectEntries.Count)
+            Assert.Equal("B", ClsVBEH.ProjectEntries(1).ProjectName)
+            Assert.Equal("CDB", ClsVBEH.ProjectEntries(1).CodeDirectrory)
 
-        ClsVBEH.ProjectAdd("A", "CDAA")
+            ClsVBEH.ProjectAdd("A", "CDAA")
 
-        Assert.Equal(2, ClsVBEH.ProjectEntries.Count)
-        Assert.Equal("A", ClsVBEH.ProjectEntries(ClsVBEH.ProjectEntries.Count - 1).ProjectName)
-        Assert.Equal("CDAA", ClsVBEH.ProjectEntries(ClsVBEH.ProjectEntries.Count - 1).CodeDirectrory)
+            Assert.Equal(2, ClsVBEH.ProjectEntries.Count)
+            Assert.Equal("A", ClsVBEH.ProjectEntries(ClsVBEH.ProjectEntries.Count - 1).ProjectName)
+            Assert.Equal("CDAA", ClsVBEH.ProjectEntries(ClsVBEH.ProjectEntries.Count - 1).CodeDirectrory)
 
-    End Sub
+        End Sub
 
-    <Fact>
-    Sub TestGetProjectDirectory()
-        ClsVBEH.ProjectEntries.Clear()
+        <Fact>
+        Sub TestGetProjectDirectory()
+            ClsVBEH.ProjectEntries.Clear()
 
-        ClsVBEH.ProjectAdd("A", "CDA")
-        ClsVBEH.ProjectAdd("B", "CDB")
+            ClsVBEH.ProjectAdd("A", "CDA")
+            ClsVBEH.ProjectAdd("B", "CDB")
 
-        Assert.Equal("CDA", ClsVBEH.ProjectDirectoryByName("A"))
-        Assert.Equal("CDB", ClsVBEH.ProjectDirectoryByName("B"))
-        Assert.Null(ClsVBEH.ProjectDirectoryByName("C"))
-        Assert.Null(ClsVBEH.ProjectDirectoryByName("D"))
+            Assert.Equal("CDA", ClsVBEH.ProjectDirectoryByName("A"))
+            Assert.Equal("CDB", ClsVBEH.ProjectDirectoryByName("B"))
+            Assert.Null(ClsVBEH.ProjectDirectoryByName("C"))
+            Assert.Null(ClsVBEH.ProjectDirectoryByName("D"))
 
-        ClsVBEH.ProjectAdd("C", "CDC")
-        Assert.Equal("CDA", ClsVBEH.ProjectDirectoryByName("A"))
-        Assert.Equal("CDB", ClsVBEH.ProjectDirectoryByName("B"))
-        Assert.Equal("CDC", ClsVBEH.ProjectDirectoryByName("C"))
-        Assert.Null(ClsVBEH.ProjectDirectoryByName("D"))
-    End Sub
+            ClsVBEH.ProjectAdd("C", "CDC")
+            Assert.Equal("CDA", ClsVBEH.ProjectDirectoryByName("A"))
+            Assert.Equal("CDB", ClsVBEH.ProjectDirectoryByName("B"))
+            Assert.Equal("CDC", ClsVBEH.ProjectDirectoryByName("C"))
+            Assert.Null(ClsVBEH.ProjectDirectoryByName("D"))
+        End Sub
 
-    <Fact>
-    Sub TestWriteProjectDirectory()
-        ClsVBEH.ProjectEntries.Clear()
-        ClsVBEH.ProjectAdd("A", "CDA")
-        ClsVBEH.ProjectAdd("B", "CDB")
-        ClsVBEH.ProjectAdd("C", "CDC")
+        <Fact>
+        Sub TestWriteProjectDirectory()
+            ClsVBEH.ProjectEntries.Clear()
+            ClsVBEH.ProjectAdd("A", "CDA")
+            ClsVBEH.ProjectAdd("B", "CDB")
+            ClsVBEH.ProjectAdd("C", "CDC")
 
-        Dim strPath As String = Path.Combine(TestFolder, "WriteProject.txt")
-        File.Delete(strPath)
-        ClsVBEH.WriteProjectEntries(strPath)
+            Dim strPath As String = Path.Combine(TestFolder, "WriteProject.txt")
+            File.Delete(strPath)
+            ClsVBEH.WriteProjectEntries(strPath)
 
-        Using sr As New StreamReader(strPath)
-            Assert.Equal("A; CDA", sr.ReadLine())
-            Assert.Equal("B; CDB", sr.ReadLine())
-            Assert.Equal("C; CDC", sr.ReadLine())
-        End Using
-    End Sub
+            Using sr As New StreamReader(strPath)
+                Assert.Equal("A; CDA", sr.ReadLine())
+                Assert.Equal("B; CDB", sr.ReadLine())
+                Assert.Equal("C; CDC", sr.ReadLine())
+            End Using
+        End Sub
 
-    <Fact>
-    Sub TestReadProjectDirectory()
-        ClsVBEH.ProjectEntries.Clear()
+        <Fact>
+        Sub TestReadProjectDirectory()
+            ClsVBEH.ProjectEntries.Clear()
 
-        Dim strPath As String = Path.Combine(TestFolder, "WriteProject.txt")
-        File.Delete(strPath)
-        Using sw As New StreamWriter(strPath)
+            Dim strPath As String = Path.Combine(TestFolder, "WriteProject.txt")
+            File.Delete(strPath)
+            Using sw As New StreamWriter(strPath)
 
-            sw.WriteLine("A; TA")
-            sw.WriteLine("B; TB")
-            sw.WriteLine("C; TC")
-        End Using
+                sw.WriteLine("A; TA")
+                sw.WriteLine("B; TB")
+                sw.WriteLine("C; TC")
+            End Using
 
-        ClsVBEH.ReadProjectEntries(strPath)
+            ClsVBEH.ReadProjectEntries(strPath)
 
-        Assert.Equal(3, ClsVBEH.ProjectEntries.Count)
-        Assert.Equal("A", ClsVBEH.ProjectEntries(0).ProjectName)
-        Assert.Equal("TA", ClsVBEH.ProjectEntries(0).CodeDirectrory)
-        Assert.Equal("B", ClsVBEH.ProjectEntries(1).ProjectName)
-        Assert.Equal("TB", ClsVBEH.ProjectEntries(1).CodeDirectrory)
-        Assert.Equal("C", ClsVBEH.ProjectEntries(2).ProjectName)
-        Assert.Equal("TC", ClsVBEH.ProjectEntries(2).CodeDirectrory)
-    End Sub
+            Assert.Equal(3, ClsVBEH.ProjectEntries.Count)
+            Assert.Equal("A", ClsVBEH.ProjectEntries(0).ProjectName)
+            Assert.Equal("TA", ClsVBEH.ProjectEntries(0).CodeDirectrory)
+            Assert.Equal("B", ClsVBEH.ProjectEntries(1).ProjectName)
+            Assert.Equal("TB", ClsVBEH.ProjectEntries(1).CodeDirectrory)
+            Assert.Equal("C", ClsVBEH.ProjectEntries(2).ProjectName)
+            Assert.Equal("TC", ClsVBEH.ProjectEntries(2).CodeDirectrory)
+        End Sub
 
 
-End Class
+    End Class
+End Namespace
